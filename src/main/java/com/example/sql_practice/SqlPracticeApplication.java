6b5240be428f5e22c2e24419ae1b6acb56967628
Sofia <sofia@dtonic.io>
2022-12-08 19:41:55 +0900

    spring init

---
+++ src/main/java/com/example/sql_practice/SqlPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.sql_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sql_practice/SqlPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sql_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
